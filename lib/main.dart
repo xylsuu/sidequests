6fef0d4fef013ccae84ed02057c289262239a4fb
DKXLY <77124386+DKXLY@users.noreply.github.com>
2024-09-02 23:31:38 +0200

    idk

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,7 @@ class MyApp extends StatelessWidget {
         title: 'Sidequests',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple
-          
-          ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
         home: MyHomePage(),
       ),
@@ -31,10 +29,17 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var liked = List.empty();
 
   void getNext() {
     current = WordPair.random();
     notifyListeners();
+  }
+
+  void likeCurrent() {
+    if (!liked.contains(current))
+    liked.add(current);
+
   }
 }
 
@@ -43,17 +48,31 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+    var liked = appState.liked;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Flutter je crazy:'),
-          BigCard(pair: pair),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
 
-          ElevatedButton(onPressed: () {
-            appState.getNext();
-          }, child: Text('NEXT'))
-        ],
+          children: [
+            Text('Flutter je crazy:'),
+            BigCard(pair: pair),
+        
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              }, 
+              child: Text('NEXT')
+            )
+
+            ElevatedButton(
+              onPressed: ,
+              child: child
+            )
+          ],
+          
+        ),
       ),
     );
   }
@@ -62,23 +81,30 @@ class MyHomePage extends StatelessWidget {
 class BigCard extends StatelessWidget {
   const BigCard({
     super.key,
+    
     required this.pair,
   });
 
+  
   final WordPair pair;
   
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontFamily: 'Times New Roman',
+    );
 
     return Card(
+      elevation: 30,
+      
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(50),
         child: Text(
           pair.asLowerCase,
-          style: TextStyle(color: Colors.white),
+          style: style,
         )
       ),
     );
